926918340fc9a364f731128516aa767377ec2ebd
Johannes Werner <johannes.werner86@gmail.com>
2019-08-06 17:27:24 +0200

    initial commit

---
+++ .vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <article>
+    <Content/>
+  </article>
+</template>
+
+<script>
+export default {
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ README.md
@@ -0,0 +1,5 @@
+---
+home: true
+layout: Home
+---
+# coming soon
